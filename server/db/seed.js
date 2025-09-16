376e626e97601883aac6e617088199429a440277
milkBread01 <guzkevin@gmail.com>
2025-09-15 23:54:56 -0400

    syntax err

--- server/db/seed.js
+++ server/db/seed.js
@@ -4,118 +4,284 @@ import { faker } from '@faker-js/faker';
 import bcrypt from 'bcryptjs';
 
 // Helper function to generate chess board state as JSON
-const generateInitialBoard = () => {
-    const board = {};
-    
-    // White pieces
-    board["a1"] = { id: "wr-1", type: "rook", color: "white", img: "/chess/assets/white-rook.png", moves: 0, location: "a1" };
-    board["b1"] = { id: "wn-1", type: "knight", color: "white", img: "/chess/assets/white-knight.png", moves: 0, location: "b1" };
-    board["c1"] = { id: "wb-1", type: "bishop", color: "white", img: "/chess/assets/white-bishop.png", moves: 0, location: "c1" };
-    board["d1"] = { id: "wq-1", type: "queen", color: "white", img: "/chess/assets/white-queen.png", moves: 0, location: "d1" };
-    board["e1"] = { id: "wk-1", type: "king", color: "white", img: "/chess/assets/white-king.png", moves: 0, location: "e1" };
-    board["f1"] = { id: "wb-2", type: "bishop", color: "white", img: "/chess/assets/white-bishop.png", moves: 0, location: "f1" };
-    board["g1"] = { id: "wn-2", type: "knight", color: "white", img: "/chess/assets/white-knight.png", moves: 0, location: "g1" };
-    board["h1"] = { id: "wr-2", type: "rook", color: "white", img: "/chess/assets/white-rook.png", moves: 0, location: "h1" };
-    
-    // White pawns
-    for (let i = 0; i < 8; i++) {
-        const file = String.fromCharCode(97 + i); // 'a' through 'h'
-        board[`${file}2`] = { 
-            id: `wp-${i + 1}`, 
-            type: "pawn", 
-            color: "white", 
-            img: "/chess/assets/white-pawn.png", 
-            moves: 0, 
-            location: `${file}2` 
-        };
-    }
-    
-    // Black pieces
-    board["a8"] = { id: "br-1", type: "rook", color: "black", img: "/chess/assets/black-rook.png", moves: 0, location: "a8" };
-    board["b8"] = { id: "bn-1", type: "knight", color: "black", img: "/chess/assets/black-knight.png", moves: 0, location: "b8" };
-    board["c8"] = { id: "bb-1", type: "bishop", color: "black", img: "/chess/assets/black-bishop.png", moves: 0, location: "c8" };
-    board["d8"] = { id: "bq-1", type: "queen", color: "black", img: "/chess/assets/black-queen.png", moves: 0, location: "d8" };
-    board["e8"] = { id: "bk-1", type: "king", color: "black", img: "/chess/assets/black-king.png", moves: 0, location: "e8" };
-    board["f8"] = { id: "bb-2", type: "bishop", color: "black", img: "/chess/assets/black-bishop.png", moves: 0, location: "f8" };
-    board["g8"] = { id: "bn-2", type: "knight", color: "black", img: "/chess/assets/black-knight.png", moves: 0, location: "g8" };
-    board["h8"] = { id: "br-2", type: "rook", color: "black", img: "/chess/assets/black-rook.png", moves: 0, location: "h8" };
-    
-    // Black pawns
-    for (let i = 0; i < 8; i++) {
-        const file = String.fromCharCode(97 + i); // 'a' through 'h'
-        board[`${file}7`] = { 
-            id: `bp-${i + 1}`, 
-            type: "pawn", 
-            color: "black", 
-            img: "/chess/assets/black-pawn.png", 
-            moves: 0, 
-            location: `${file}7` 
-        };
-    }
-    
-    return board;
-};
-
-// Helper function to simulate a chess game by making random moves
-const generateChessGameMoves = (numMoves) => {
-    const board = generateInitialBoard();
-    const moves = [JSON.parse(JSON.stringify(board))]; // Save initial position
-    
-    // Get all possible squares
-    const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-    const ranks = ['1', '2', '3', '4', '5', '6', '7', '8'];
-    const allSquares = [];
-    files.forEach(file => ranks.forEach(rank => allSquares.push(file + rank)));
-    
-    // Simulate moves by randomly moving pieces
-    for (let i = 0; i < numMoves && i < 100; i++) {
-        const occupiedSquares = Object.keys(board);
-        if (occupiedSquares.length === 0) break;
-        
-        // Pick a random piece to move
-        const fromSquare = faker.helpers.arrayElement(occupiedSquares);
-        const piece = board[fromSquare];
-        
-        // Pick a random destination (simulate move)
-        const toSquare = faker.helpers.arrayElement(allSquares);
-        
-        // Don't move to same square or to a square with same color piece
-        if (toSquare === fromSquare || 
-            (board[toSquare] && board[toSquare].color === piece.color)) {
-            continue;
+function generateChessGameMoves () {
+    const moves = {
+        "a7": {
+            "id": "bp-2",
+            "type": "pawn",
+            "color": "black",
+            "img": "/chess/assets/black-pawn.png",
+            "moves": 0,
+            "location": "a7"
+        },
+        "a1": {
+            "id": "wr-3",
+            "type": "rook",
+            "color": "white",
+            "img": "/chess/assets/white-rook.png",
+            "moves": 0,
+            "location": "a1"
+        },
+        "a8": {
+            "id": "br-4",
+            "type": "rook",
+            "color": "black",
+            "img": "/chess/assets/black-rook.png",
+            "moves": 0,
+            "location": "a8"
+        },
+        "b2": {
+            "id": "wp-5",
+            "type": "pawn",
+            "color": "white",
+            "img": "/chess/assets/white-pawn.png",
+            "moves": 0,
+            "location": "b2"
+        },
+        "c2": {
+            "id": "wp-9",
+            "type": "pawn",
+            "color": "white",
+            "img": "/chess/assets/white-pawn.png",
+            "moves": 0,
+            "location": "c2"
+        },
+        "c1": {
+            "id": "wb-11",
+            "type": "bishop",
+            "color": "white",
+            "img": "/chess/assets/white-bishop.png",
+            "moves": 0,
+            "location": "c1"
+        },
+        "c8": {
+            "id": "bb-12",
+            "type": "bishop",
+            "color": "black",
+            "img": "/chess/assets/black-bishop.png",
+            "moves": 0,
+            "location": "c8"
+        },
+        "d2": {
+            "id": "wp-13",
+            "type": "pawn",
+            "color": "white",
+            "img": "/chess/assets/white-pawn.png",
+            "moves": 0,
+            "location": "d2"
+        },
+        "d1": {
+            "id": "wq-15",
+            "type": "queen",
+            "color": "white",
+            "img": "/chess/assets/white-queen.png",
+            "moves": 0,
+            "location": "d1"
+        },
+        "d8": {
+            "id": "bq-16",
+            "type": "queen",
+            "color": "black",
+            "img": "/chess/assets/black-queen.png",
+            "moves": 0,
+            "location": "d8"
+        },
+        "e7": {
+            "id": "bp-18",
+            "type": "pawn",
+            "color": "black",
+            "img": "/chess/assets/black-pawn.png",
+            "moves": 0,
+            "location": "e7"
+        },
+        "e1": {
+            "id": "wk-19",
+            "type": "king",
+            "color": "white",
+            "img": "/chess/assets/white-king.png",
+            "moves": 0,
+            "location": "e1"
+        },
+        "e8": {
+            "id": "bk-20",
+            "type": "king",
+            "color": "black",
+            "img": "/chess/assets/black-king.png",
+            "moves": 0,
+            "location": "e8"
+        },
+        "f2": {
+            "id": "wp-21",
+            "type": "pawn",
+            "color": "white",
+            "img": "/chess/assets/white-pawn.png",
+            "moves": 0,
+            "location": "f2"
+        },
+        "f7": {
+            "id": "bp-22",
+            "type": "pawn",
+            "color": "black",
+            "img": "/chess/assets/black-pawn.png",
+            "moves": 0,
+            "location": "f7"
+        },
+        "f1": {
+            "id": "wb-23",
+            "type": "bishop",
+            "color": "white",
+            "img": "/chess/assets/white-bishop.png",
+            "moves": 0,
+            "location": "f1"
+        },
+        "f8": {
+            "id": "bb-24",
+            "type": "bishop",
+            "color": "black",
+            "img": "/chess/assets/black-bishop.png",
+            "moves": 0,
+            "location": "f8"
+        },
+        "g2": {
+            "id": "wp-25",
+            "type": "pawn",
+            "color": "white",
+            "img": "/chess/assets/white-pawn.png",
+            "moves": 0,
+            "location": "g2"
+        },
+        "g7": {
+            "id": "bp-26",
+            "type": "pawn",
+            "color": "black",
+            "img": "/chess/assets/black-pawn.png",
+            "moves": 0,
+            "location": "g7"
+        },
+        "g1": {
+            "id": "wk-27",
+            "type": "knight",
+            "color": "white",
+            "img": "/chess/assets/white-knight.png",
+            "moves": 0,
+            "location": "g1"
+        },
+        "g8": {
+            "id": "bk-28",
+            "type": "knight",
+            "color": "black",
+            "img": "/chess/assets/black-knight.png",
+            "moves": 0,
+            "location": "g8"
+        },
+        "h2": {
+            "id": "wp-29",
+            "type": "pawn",
+            "color": "white",
+            "img": "/chess/assets/white-pawn.png",
+            "moves": 0,
+            "location": "h2"
+        },
+        "h7": {
+            "id": "bp-30",
+            "type": "pawn",
+            "color": "black",
+            "img": "/chess/assets/black-pawn.png",
+            "moves": 0,
+            "location": "h7"
+        },
+        "h1": {
+            "id": "wr-31",
+            "type": "rook",
+            "color": "white",
+            "img": "/chess/assets/white-rook.png",
+            "moves": 0,
+            "location": "h1"
+        },
+        "h8": {
+            "id": "br-32",
+            "type": "rook",
+            "color": "black",
+            "img": "/chess/assets/black-rook.png",
+            "moves": 0,
+            "location": "h8"
+        },
+        "e4": {
+            "id": "wp-17",
+            "type": "pawn",
+            "color": "white",
+            "img": "/chess/assets/white-pawn.png",
+            "moves": 1,
+            "location": "e4"
+        },
+        "d5": {
+            "id": "bp-14",
+            "type": "pawn",
+            "color": "black",
+            "img": "/chess/assets/black-pawn.png",
+            "moves": 1,
+            "location": "d5"
+        },
+        "c3": {
+            "id": "wk-7",
+            "type": "knight",
+            "color": "white",
+            "img": "/chess/assets/white-knight.png",
+            "moves": 1,
+            "location": "c3"
+        },
+        "c6": {
+            "id": "bp-10",
+            "type": "pawn",
+            "color": "black",
+            "img": "/chess/assets/black-pawn.png",
+            "moves": 1,
+            "location": "c6"
+        },
+        "b5": {
+            "id": "bp-6",
+            "type": "pawn",
+            "color": "black",
+            "img": "/chess/assets/black-pawn.png",
+            "moves": 1,
+            "location": "b5"
+        },
+        "a5": {
+            "id": "wp-1",
+            "type": "pawn",
+            "color": "white",
+            "img": "/chess/assets/white-pawn.png",
+            "moves": 2,
+            "location": "a5"
+        },
+        "a6": {
+            "id": "bk-8",
+            "type": "knight",
+            "color": "black",
+            "img": "/chess/assets/black-knight.png",
+            "moves": 1,
+            "location": "a6"
         }
-        
-        // Make the move
-        delete board[fromSquare];
-        board[toSquare] = {
-            ...piece,
-            location: toSquare,
-            moves: piece.moves + 1
-        };
-        
-        // Save this position
-        moves.push(JSON.parse(JSON.stringify(board)));
     }
     
     return JSON.stringify(moves);
 };
 
-// Helper function to generate usernames that match the regex
-const generateUsername = () => {
+// generate usernames that match the regex
+function generateUsername () {
     const username = faker.person.firstName().toLowerCase() + faker.number.int({ min: 100, max: 999 }).toString();
     return username.length >= 5 ? username.substring(0, 24) : username.padEnd(5, '0');
 };
 
-// Helper function to generate names that match the regex
-const generateName = () => {
+// generate names that match the regex
+function generateName () {
     return faker.person.firstName().replace(/[^A-Za-z]/g, '');
 };
 
-// Helper function to generate passwords that match the regex
-const generatePassword = () => {
+// generate passwords that match the regex
+function generatePassword() {
     const basePassword = faker.internet.password(12, false);
-    const specialChars = '!@#$%^&*(),.?":{}|<>';
-    const specialChar = specialChars[Math.floor(Math.random() * specialChars.length)];
+    const specialChar = "$";
     return basePassword + specialChar;
 };
 
@@ -123,7 +289,6 @@ const seed = async () => {
     try {
         console.log("Starting database seeding...");
 
-        // Clear existing data (optional - comment out if you want to keep existing data)
         await client.query("DELETE FROM game_history");
         await client.query("DELETE FROM user_records");
         await client.query("DELETE FROM user_accounts");
@@ -154,7 +319,7 @@ const seed = async () => {
                 username,
                 email,
                 password_hash,
-                faker.datatype.boolean(0.7) // 70% chance of being verified
+                faker.datatype.boolean(0.5)
             ]);
 
             const user = userResult.rows[0];
@@ -202,7 +367,7 @@ const seed = async () => {
                 const black_player_name = white_player_name === user.username ? faker.person.firstName() : user.username;
                 const game_result = faker.helpers.arrayElement(gameResults);
                 const numMoves = faker.number.int({ min: 10, max: 50 });
-                const moves = generateChessGameMoves(numMoves);
+                const moves = numMoves;
 
                 const gameSQL = `
                     INSERT INTO game_history (account_owner_id, white_player_name, black_player_name, game_result, moves)
